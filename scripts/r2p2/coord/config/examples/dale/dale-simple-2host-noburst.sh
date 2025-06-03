1136b4aa6bd2d29fa0605a51baf6654dc2f01f37
Dale Huang <huangzihan98@gmail.com>
2025-06-03 18:51:13 +0100

    specify 2host sim with burst (1st part of msg) and no burst

--- scripts/r2p2/coord/config/examples/dale/dale-simple-2host-noburst.sh
+++ scripts/r2p2/coord/config/examples/dale/dale-simple-2host-noburst.sh
@@ -15,7 +15,7 @@ state_polling_ival_s='0.000001' # sample switch queue lengths every 1us
 req_interval_distr='manual'
 req_target_distr='manual' # manual or uniform
 req_size_distr='manual'
-manual_req_interval_file_l='dale-simple-pair.csv'
+manual_req_interval_file_l='dale-simple-2host.csv'
 client_injection_rate_gbps_list='60' # value is meaningful only when req_interval_distr is not 'manual'
 duration_modifier_l='0.0004' # when manual, this specifies the sim duration in secods. For 3 1MB flows at 100Gbps, it takes approx 3*80us = 0.00024sec
 mean_req_size_B_l='121848' # only meanigful if req_size_distr is not manual
@@ -26,7 +26,7 @@ global_debug='6' # From 1 to 7 (7 = super verbose). Set it to 0 to enable the fo
 # ===================== R2P2 Parameters =====================
 r2p2_budgets_intra_max_bytes_l='108000'
 r2p2_elet_srpb_l='72000'
-r2p2_unsolicited_thresh_bytes_l="0"
+r2p2_unsolicited_thresh_bytes_l="0" # here we set UnschT = 0 i.e. no bursting.
 r2p2_hybrid_sender_policy_l="1"
 r2p2_sender_policy_ratio_l='0.5'
 r2p2_sender_ecn_threshold_l="25"
