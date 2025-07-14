a611a10fe5b720bbf5d25c8a7fced5b62e18e052
Dale Huang <huangzihan98@gmail.com>
2025-07-14 15:42:25 +0100

    WIP: qts thrpt copmarison python script
      * TODO: create plots

--- scripts/r2p2/post-process/dale_compare_thrpt_vs_gdpt.py
+++ scripts/r2p2/post-process/dale_compare_thrpt_vs_gdpt.py
@@ -33,6 +33,18 @@ class QmonResults:
         # TODO
         pass
 
+def get_qts_timestamp_cutoff_from_csv(path_to_qts_csv):
+    with open(path_to_qts_csv, 'r') as file:
+        prev_timestamp_s = None
+        for row in reversed(list(csv.reader(file, delimiter=","))):
+            timestamp_s = float(row[0])
+            thrpt_gbps = float(row[1])
+            qing_KB = float(row[2])
+            if (thrpt_gbps == 0 and qing_KB == 0):
+                prev_timestamp_s = timestamp_s
+            else:
+                return prev_timestamp_s if prev_timestamp_s else timestamp_s
+
 def get_qts_results_from_csv(nw_elem, src, dst, path_to_qts_csv, timestamp_cutoff_s=None):
     with open(path_to_qts_csv, newline='') as file:
         reader = csv.reader(file, delimiter=',')
@@ -50,27 +62,31 @@ def get_qts_results_from_csv(nw_elem, src, dst, path_to_qts_csv, timestamp_cutof
 
         return QmonResults(nw_elem, src, dst, timestamps_list, throughput_gbps_list, queueing_KB_list) 
 
-def get_qts_result(proto, nw_elem, src, dst, num_flows, num_byteloads_per_flow, byteload_size_B, inter_byteload_period_us, timestamp_cutoff_s=None, title_addendum=""):
+def get_qts_result_path(proto, nw_elem, src, dst, num_flows, num_byteloads_per_flow, byteload_size_B, inter_byteload_period_us, title_addendum = ""):
     if proto.upper() == SSIRD_PROTO_NAME: 
-        path_to_qts_results = f"{PATH_TO_SIM_RESULTS}{SSIRD_PROTO_NAME}-{num_flows}flo-{num_byteloads_per_flow}#-{byteload_size_B}B-{inter_byteload_period_us}us{title_addendum}/data/{SSIRD_PROTO_NAME}/{CLIENT_INJECTION_RATE_GBPS}/output/qts/{nw_elem}/qts_{src}_{dst}.csv"
+        return f"{PATH_TO_SIM_RESULTS}{SSIRD_PROTO_NAME}-{num_flows}flo-{num_byteloads_per_flow}#-{byteload_size_B}B-{inter_byteload_period_us}us{title_addendum}/data/{SSIRD_PROTO_NAME}/{CLIENT_INJECTION_RATE_GBPS}/output/qts/{nw_elem}/qts_{src}_{dst}.csv"
 
     elif proto.upper() == DCTCP_PROTO_NAME:
-        path_to_qts_results = f"{PATH_TO_SIM_RESULTS}{DCTCP_PROTO_NAME}-{DCTCP_ECN_THRESH}-{num_flows}flo-{num_byteloads_per_flow}#-{byteload_size_B}B-{inter_byteload_period_us}us{title_addendum}/data/{DCTCP_PROTO_NAME}-{DCTCP_ECN_THRESH}/{CLIENT_INJECTION_RATE_GBPS}/output/qts/{nw_elem}/qts_{src}_{dst}.csv"
+        return f"{PATH_TO_SIM_RESULTS}{DCTCP_PROTO_NAME}-{DCTCP_ECN_THRESH}-{num_flows}flo-{num_byteloads_per_flow}#-{byteload_size_B}B-{inter_byteload_period_us}us{title_addendum}/data/{DCTCP_PROTO_NAME}-{DCTCP_ECN_THRESH}/{CLIENT_INJECTION_RATE_GBPS}/output/qts/{nw_elem}/qts_{src}_{dst}.csv"
 
     else:
         print(f"ERROR: proto name '{proto}' unrecognised!")
 
-    return get_qts_results_from_csv(nw_elem, src, dst, path_to_qts_results, timestamp_cutoff_s)
+def get_qts_result_ssird_dctcp(nw_elem, src, dst, num_flows, num_byteloads_per_flow, byteload_size_B, inter_byteload_period_us, title_addendum=""):
+    ssird_qts_result_path = get_qts_result_path(SSIRD_PROTO_NAME, nw_elem, src, dst, num_flows, num_byteloads_per_flow, byteload_size_B, inter_byteload_period_us, title_addendum)
 
+    dctcp_qts_result_path = get_qts_result_path(DCTCP_PROTO_NAME, nw_elem, src, dst, num_flows, num_byteloads_per_flow, byteload_size_B, inter_byteload_period_us, title_addendum)
 
-if __name__ == "__main__":
-   
-    num_flows = 2
-    num_byteloads_per_flow_list = [10, 10, 10, 10] # should be [10000, 1000, 100, 10] but there was a naming bug in the experiment
-    byteload_size_B_list = [4, 40, 400, 4000]
-    inter_byteload_period_us_list = [1, 10, 100, 1000]
-    timestamp_cutoff_us_list = [0.020006, 0.009998, 0.009913, 0.009008]
+    ssird_timestamp_cutoff_s = get_qts_timestamp_cutoff_from_csv(ssird_qts_result_path)
+    dctcp_timestamp_cutoff_s = get_qts_timestamp_cutoff_from_csv(dctcp_qts_result_path)
+    overall_timestamp_cutoff_s = max(ssird_timestamp_cutoff_s, dctcp_timestamp_cutoff_s)
 
+    ssird_qts_results_obj = get_qts_results_from_csv(nw_elem, src, dst, ssird_qts_result_path, overall_timestamp_cutoff_s)
+    dctcp_qts_results_obj = get_qts_results_from_csv(nw_elem, src, dst, dctcp_qts_result_path, overall_timestamp_cutoff_s)
+
+    return ssird_qts_results_obj, dctcp_qts_results_obj
+
+def get_qts_thrpt_diff_ssird_dctcp(num_flows, num_byteloads_per_flow_list, byteload_size_B_list, inter_byteload_period_us_list):
     num_experiments = len(num_byteloads_per_flow_list)
     assert(len(byteload_size_B_list) == num_experiments)
     assert(len(inter_byteload_period_us_list) == num_experiments)
@@ -78,28 +94,31 @@ if __name__ == "__main__":
     ssird_qts_results_list = []
     dctcp_qts_results_list = []
     for i in range(0, num_experiments):
-        ssird_subpkt_result = get_qts_result(SSIRD_PROTO_NAME, HOST, "host_0", "tor_4", num_flows, num_byteloads_per_flow_list[i], byteload_size_B_list[i], inter_byteload_period_us_list[i], timestamp_cutoff_us_list[i], title_addendum="_subpkt_multiflow") 
-        dctcp_subpkt_result = get_qts_result(DCTCP_PROTO_NAME, HOST, "host_0", "tor_4", num_flows, num_byteloads_per_flow_list[i], byteload_size_B_list[i], inter_byteload_period_us_list[i], timestamp_cutoff_us_list[i], title_addendum="_subpkt_multiflow") 
+        ssird_subpkt_result, dctcp_subpkt_result = get_qts_result_ssird_dctcp(HOST, "host_0", "tor_4", num_flows, num_byteloads_per_flow_list[i], byteload_size_B_list[i], inter_byteload_period_us_list[i], title_addendum="_subpkt_multiflow") 
         ssird_qts_results_list.append(ssird_subpkt_result)
         dctcp_qts_results_list.append(dctcp_subpkt_result)
 
     ssird_thrpt_gbps_list = [x.get_avg_thrpt_gbps() for x in ssird_qts_results_list]
     dctcp_thrpt_gbps_list = [x.get_avg_thrpt_gbps() for x in dctcp_qts_results_list]
     diff_thrpt_gbps_list = [s - d for s, d in zip(ssird_thrpt_gbps_list, dctcp_thrpt_gbps_list)]
+
+    return ssird_thrpt_gbps_list, dctcp_thrpt_gbps_list, diff_thrpt_gbps_list
+
+def compare_thrpt_subpkt_multiflow_4B_to_4000B_10flo():
+    num_flows = 2
+    num_byteloads_per_flow_list = [10, 10, 10, 10] # should be [10000, 1000, 100, 10] but there was a naming bug in the experiment
+    byteload_size_B_list = [4, 40, 400, 4000]
+    inter_byteload_period_us_list = [1, 10, 100, 1000]
+
+    theoretical_goodput_gbps = num_flows * max(byteload_size_B_list) * 8 / (max(inter_byteload_period_us_list) * pow(10,-6)) * pow(10,-9)
+
+    ssird_thrpt_gbps_list, dctcp_thrpt_gbps_list, diff_thrpt_gbps_list = get_qts_thrpt_diff_ssird_dctcp(num_flows, num_byteloads_per_flow_list, byteload_size_B_list, inter_byteload_period_us_list)
+
+    print(theoretical_goodput_gbps)
     print(ssird_thrpt_gbps_list)
     print(dctcp_thrpt_gbps_list)
     print(diff_thrpt_gbps_list)
 
-    # ---
-    # ssird_subpkt_result = get_qts_result(SSIRD_PROTO_NAME, HOST, "host_0", "tor_4", 2, 10, 4, 1, 0.020006, title_addendum="_subpkt_multiflow") 
-    # dctcp_subpkt_result = get_qts_result(DCTCP_PROTO_NAME, HOST, "host_0", "tor_4", 2, 10, 4, 1, 0.020006, title_addendum="_subpkt_multiflow") 
-    # print(ssird_subpkt_result.get_avg_thrpt_gbps())
-    # print(dctcp_subpkt_result.get_avg_thrpt_gbps())
+if __name__ == "__main__":
+    compare_thrpt_subpkt_multiflow_4B_to_4000B_10flo()
 
-    # ---
-    # ssird_subpkt_2flo_10bload_4B_1us_qmon_path = f"{PATH_TO_SIM_RESULTS}SSIRD-2flo-10#-4B-1us_subpkt_multiflow/data/SSIRD/60/output/qts/host/qts_host_0_tor_4.csv"
-    # ssird_subpkt_2flo_10bload_4B_1us_qmon_result = get_qmon_results_from_csv(HOST, "host_0", "tor_4", ssird_subpkt_2flo_10bload_4B_1us_qmon_path, 0.020006)
-    # print(ssird_subpkt_2flo_10bload_4B_1us_qmon_result.get_avg_thrpt_gbps())
-    # dctcp_subpkt_2flo_10bload_4B_1us_qmon_path = f"{PATH_TO_SIM_RESULTS}DCTCP-50-2flo-10#-4B-1us_subpkt_multiflow/data/DCTCP-50/60/output/qts/host/qts_host_0_tor_4.csv"
-    # dctcp_subpkt_2flo_10bload_4B_1us_qmon_result = get_qmon_results_from_csv(HOST, "host_0", "tor_4", dctcp_subpkt_2flo_10bload_4B_1us_qmon_path, 0.020006)
-    # print(dctcp_subpkt_2flo_10bload_4B_1us_qmon_result.get_avg_thrpt_gbps())
