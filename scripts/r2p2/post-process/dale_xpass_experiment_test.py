7c79091904565d616ab50779167b9c05efa168d6
dalehuangzihan <huangzihan98@gmail.com>
2025-08-19 09:17:00 +0100

    update dale_xpass_experiment_test.py

--- scripts/r2p2/post-process/dale_xpass_experiment_test.py
+++ scripts/r2p2/post-process/dale_xpass_experiment_test.py
@@ -245,24 +245,45 @@ if __name__ == "__main__":
     #     experiment_date=dale_experiment_rig.Experiment.get_date_now_formatted()
     # ) 
 
-    run_experiment_from_saved_json(
-        saved_json_file="xpass_stresstest_3to1_4host_GoogleAllRPC_loadtest_2025-08-18T_14-32-25Z.json",
+    dale_final_eval_experiments.run_experiment(
         proto_names = [dale_experiment_rig.DCTCP_PROTO_NAME, dale_experiment_rig.XPASS_PROTO_NAME],
         topo_yaml_file='4-hosts.yaml',
         src_dst_pairs_list=[(1,0), (2,0), (3,0)],
         num_flows_list=[20],
         byteload_size_B_list=[1458],
         target_mean_byteload_interval_nanosec_list=[1000],
+        max_interval_nanosec_list=[10000],
         flow_size_distr_list=[dale_experiment_rig.WxDistr(cdf_file_name="Google_AllRPC.txt")],
         target_mean_flow_interarr_ns=1000,
         is_use_poisson_byteload_intervals=True,
         is_use_poisson_flow_interarr=True,
-        ssird_sim_dur_list=[0.01],
-        dctcp_sim_dur_list=[0.01],
-        xpass_sim_dur_list=[0.01],
+        ssird_sim_dur_list=[0.005],
+        dctcp_sim_dur_list=[0.005],
+        xpass_sim_dur_list=[0.005],
         is_full_postproc=True,
         title_prefix="xpass_stresstest_",
         title_addendum="_4host_GoogleAllRPC_loadtest",
-        log_level=dale_experiment_rig.LOG_LEVEL_6,
-        experiment_date="2025-08-18T_14-32-25Z_redo"
+        log_level=dale_experiment_rig.LOG_LEVEL_2,
+        experiment_date=dale_experiment_rig.Experiment.get_date_now_formatted()
     ) 
+    # run_experiment_from_saved_json(
+    #     saved_json_file="xpass_stresstest_3to1_4host_GoogleAllRPC_loadtest_2025-08-18T_14-32-25Z.json",
+    #     proto_names = [dale_experiment_rig.DCTCP_PROTO_NAME, dale_experiment_rig.XPASS_PROTO_NAME],
+    #     topo_yaml_file='4-hosts.yaml',
+    #     src_dst_pairs_list=[(1,0), (2,0), (3,0)],
+    #     num_flows_list=[20],
+    #     byteload_size_B_list=[1458],
+    #     target_mean_byteload_interval_nanosec_list=[1000],
+    #     flow_size_distr_list=[dale_experiment_rig.WxDistr(cdf_file_name="Google_AllRPC.txt")],
+    #     target_mean_flow_interarr_ns=1000,
+    #     is_use_poisson_byteload_intervals=True,
+    #     is_use_poisson_flow_interarr=True,
+    #     ssird_sim_dur_list=[0.01],
+    #     dctcp_sim_dur_list=[0.01],
+    #     xpass_sim_dur_list=[0.01],
+    #     is_full_postproc=True,
+    #     title_prefix="xpass_stresstest_",
+    #     title_addendum="_4host_GoogleAllRPC_loadtest",
+    #     log_level=dale_experiment_rig.LOG_LEVEL_6,
+    #     experiment_date="2025-08-18T_14-32-25Z_redo_no_sub4_bloads"
+    # ) 
