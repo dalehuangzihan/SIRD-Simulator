f4af8127ffe676c84e625b54edc8ed811f0164d6
Dale Huang <dh1723@ic.ac.uk>
2025-08-20 16:00:40 +0000

    add script to source path environmental variables on Octopus4

---
+++ setup_paths.sh
@@ -0,0 +1,8 @@
+PATH=$PATH:/home/dh1723/SIRD-Simulator/ns2.34/bin
+PATH=$PATH:/home/dh1723/SIRD-Simulator/ns2.34/tcl8.4.18/unix
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dh1723/SIRD-Simulator/ns2.34/otcl-1.13
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dh1723/SIRD-Simulator/ns2.34/lib
+
+TCL_LIBRARY=$TCL_LIBRARY:/home/dh1723/SIRD-Simulator/ns2.34/tcl8.4.18/library
+
